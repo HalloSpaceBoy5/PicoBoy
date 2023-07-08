67d9968a42b315292f852b62dd9eb7c415a8e969
HalloSpaceBoy <111699451+HalloSpaceBoy5@users.noreply.github.com>
2023-07-08 16:48:48 -0400

    Add files via upload

--- Flappy Bird/flappybird.py
+++ Flappy Bird/flappybird.py
@@ -2,6 +2,7 @@
 from os import rename
 rename("./main.py", "./flappybird.py")
 rename("./title.py", "./main.py")
+del rename
 from PicoGameBoy import PicoGameBoy
 from time import sleep, ticks_ms, ticks_diff
 from random import randint
@@ -9,9 +10,8 @@ from _thread import start_new_thread
 from math import sqrt
 
 tick_time=0.0125
-jumpheight=5
+jumpheight=7
 fallrate=1
-pipespeed=5
 pipespace=125
 score=0
 
@@ -53,69 +53,10 @@ def sund():
             playjump=False
             sleep(0.01)
 
-def append_to_board(score):
-    with open("highscoresflappy.sc", "r") as s:
-        scores=s.read().split("\n")
-        for r in range(len(scores)):
-            scores[r]=int(scores[r])
-    newscores=scores
-    newscores.append(int(score))
-    newscores.sort(reverse=True)
-    for i in range(len(newscores)): newscores[i]=str(newscores[i])
-    with open("highscoresflappy.sc", "w+") as w:
-        w.write("\n".join(newscores[:10]))
 
-def view_scores():
-    x=open("highscoresflappy.sc", "r")
-    scores=x.read()
-    x.close()
-    del x
-    scores=scores.split("\n")
-    while True:
-        if pgb.button_B():
-            sleep(0.1)
-            return
-        if pgb.button_Home():
-            homebootstop=open("/noboot", "w")
-            homebootstop.close()
-            pgb.fill(PicoGameBoy.color(0,0,0))
-            pgb.show()
-            machine.reset()
-            break
-        pgb.fill(PicoGameBoy.color(0,0,0))
-        pgb.create_text("High Scores:", -1, 15, PicoGameBoy.color(255,255,255))
-        for i in range(len(scores)):
-            pgb.create_text("Score "+str(i+1)+": "+str(scores[i])+" Pipes", -1, 50+i*15, PicoGameBoy.color(255,255,255))
-        pgb.create_text("Press B to exit", -1, 220, PicoGameBoy.color(255,255,255))
-        pgb.show()
 
-def title_screen():
-    now = ticks_ms()
-    pgb.free_mem()
-    while True:
-        if pgb.button_Home():
-            homebootstop=open("/noboot", "w")
-            homebootstop.close()
-            pgb.fill(PicoGameBoy.color(0,0,0))
-            pgb.show()
-            machine.reset()
-            break
-        pgb.load_image("flappybird_title.bin")
-        pgb.show()
-        if pgb.button_select() or pgb.button_start():
-            view_scores()
-        elif pgb.any_button():
-            break
-        if ticks_diff(ticks_ms(), now) > 200:
-            now = ticks_ms()
-            pgb.create_text("PRESS ANY BUTTON",20, 120,PicoGameBoy.color(0,0,0))
-            pgb.create_text("TO PLAY",50, 130, PicoGameBoy.color(0,0,0))
-            pgb.show()
-            pgb.free_mem()
-            while ticks_diff(ticks_ms(), now) < 200:
-                sleep(0.020)
-                pgb.free_mem()
-            now = ticks_ms()
+
+
 
 def pause_screen():
     pgb.fill_rect(10,90,220,80,PicoGameBoy.color(0,0,0))
@@ -147,48 +88,7 @@ def check_collision(speed,detectionradius, posx, posy, width, height, posx2, pos
     
     return False
 
-def game_over():
-    global score
-    pgb.fill(PicoGameBoy.color(0,0,0))
-    pgb.create_text("GAME OVER",-1, 85,PicoGameBoy.color(255,255,255))
-    pgb.text("Press A to play again.", 35, 105, PicoGameBoy.color(255,255,255))
-    pgb.text("Press home to quit.", 40, 120, PicoGameBoy.color(255,255,255))
-    pgb.create_text("Press select/start", -1, 135, PicoGameBoy.color(255,255,255))
-    pgb.create_text("to view scores.", -1, 150, PicoGameBoy.color(255,255,255))
-    pgb.create_text("Score: "+str(int(score/7)), -1, 165, PicoGameBoy.color(255,255,255))
-    pgb.show()
-    pgb.sound(110)
-    sleep(0.05)
-    pgb.sound(0)
-    sleep(0.1)
-    pgb.sound(110)
-    sleep(0.05)
-    pgb.sound(0)
-    sleep(0.5)
-    append_to_board(score/7)
-    while True:
-        if pgb.button_Home():
-            homebootstop=open("/noboot", "w")
-            homebootstop.close()
-            pgb.fill(PicoGameBoy.color(0,0,0))
-            pgb.show()
-            machine.reset()
-            break
-        pgb.fill(PicoGameBoy.color(0,0,0))
-        pgb.create_text("GAME OVER",-1, 85,PicoGameBoy.color(255,255,255))
-        pgb.text("Press A to play again.", 35, 105, PicoGameBoy.color(255,255,255))
-        pgb.text("Press home to quit.", 40, 120, PicoGameBoy.color(255,255,255))
-        pgb.create_text("Press select/start", -1, 135, PicoGameBoy.color(255,255,255))
-        pgb.create_text("to view scores.", -1, 150, PicoGameBoy.color(255,255,255))
-        pgb.create_text("Score: "+str(int(score/7)), -1, 165, PicoGameBoy.color(255,255,255))
-        pgb.show()
-        if pgb.button_select() or pgb.button_start():
-            view_scores()
-        elif pgb.button_A():
-            score=0
-            break
-    sleep(0.5)
-    
+
 
 
 def main_game():
@@ -197,11 +97,14 @@ def main_game():
     pipepos1=240
     pipepos2=pipepos1+150
     playerspeed=0
+    pipespeed=3.5
     playerx=103
     playery=103
     score=0
     button_pressed=False
     prev_button_pressed=False
+    began=False
+    sleep(0.25)
     while True:
         if pgb.button_Home():
             homebootstop=open("/noboot", "w")
@@ -211,9 +114,28 @@ def main_game():
             machine.reset()
             break
         pgb.fill(PicoGameBoy.color(112,197,206))
+        pgb.fill_rect(0, 230, 240, 10, PicoGameBoy.color(0,154,23))
         
         #Bird jumping
         prev_button_pressed=button_pressed
+        if not began:
+            while True:
+                pgb.fill(PicoGameBoy.color(112,197,206))
+                pgb.fill_rect(0, 230, 240, 10, PicoGameBoy.color(0,154,23))
+                pgb.create_text("Press A to begin",-1,50, PicoGameBoy.color(255,255,255))
+                pgb.sprite(0, playerx, playery)
+                pgb.show()
+                if pgb.button_A():
+                    button_pressed=True
+                    began=True
+                    break
+                if pgb.button_Home():
+                    homebootstop=open("/noboot", "w")
+                    homebootstop.close()
+                    pgb.fill(PicoGameBoy.color(0,0,0))
+                    pgb.show()
+                    machine.reset()
+                    break
         if ((pgb.button_A() or pgb.button_B()) and not playery<6):
             button_pressed=True
         else:
@@ -230,7 +152,7 @@ def main_game():
         if pipepos1>=240:
             pipey1=randint(-25, 0)
             pipey11=randint(pipey1+25, pipey1+50)
-        pipepos1-=pipespeed
+        pipepos1-=int(pipespeed)
         pgb.sprite(1, pipepos1, pipey1)
         pgb.sprite(1, pipepos1, pipey11)
         pgb.sprite(2, pipepos1, pipey11+12)
@@ -243,7 +165,7 @@ def main_game():
         if pipepos2>=240:
             pipey2=randint(-25, 0)
             pipey21=randint(pipey2+25, pipey2+50)
-        pipepos2-=pipespeed
+        pipepos2-=int(pipespeed)
         pgb.sprite(1, pipepos2, pipey2)
         pgb.sprite(1, pipepos2, pipey21)
         pgb.sprite(2, pipepos2, pipey21+12)
@@ -253,7 +175,7 @@ def main_game():
         if pipepos2<-52:
             pipepos2=240
         
-        pgb.fill_rect(0, 230, 240, 10, PicoGameBoy.color(0,154,23))
+        
         
         if playery>230:
             return
@@ -263,16 +185,132 @@ def main_game():
         
         if check_collision(1, 75, playerx, playery, 34, 24, pipepos1+51, pipey11+12, 1, pipespace) or check_collision(1, 75, playerx, playery, 34, 24, pipepos2+51, pipey21+12, 1, pipespace):
             score+=1
-        pgb.create_text(str(int(score/7)), -1, 10, PicoGameBoy.color(255,255,255))
+            pipespeed+=0.005
+        pgb.create_text(str(int(score/10)), -1, 10, PicoGameBoy.color(255,255,255))
         pgb.show()
         if pgb.button_select() or pgb.button_start():
             pause_screen()
-        sleep(tick_time)
         
         
-title_screen()
+now = ticks_ms()
+while True:
+    if pgb.button_Home():
+        homebootstop=open("/noboot", "w")
+        homebootstop.close()
+        pgb.fill(PicoGameBoy.color(0,0,0))
+        pgb.show()
+        machine.reset()
+        break
+    pgb.load_image("flappybird_title.bin")
+    pgb.show()
+    if pgb.button_select() or pgb.button_start():
+            x=open("highscoresflappy.sc", "r")
+            scores=x.read()
+            x.close()
+            del x
+            scores=scores.split("\n")
+            while True:
+                if pgb.button_B():
+                    sleep(0.1)
+                    break
+                if pgb.button_Home():
+                    homebootstop=open("/noboot", "w")
+                    homebootstop.close()
+                    pgb.fill(PicoGameBoy.color(0,0,0))
+                    pgb.show()
+                    machine.reset()
+                    break
+                pgb.fill(PicoGameBoy.color(0,0,0))
+                pgb.create_text("High Scores:", -1, 15, PicoGameBoy.color(255,255,255))
+                for i in range(len(scores)):
+                    pgb.create_text("Score "+str(i+1)+": "+str(scores[i])+" Pipes", -1, 50+i*15, PicoGameBoy.color(255,255,255))
+                pgb.create_text("Press B to exit", -1, 220, PicoGameBoy.color(255,255,255))
+                pgb.show()
+            del scores
+    elif pgb.any_button():
+        break
+    if ticks_diff(ticks_ms(), now) > 200:
+        now = ticks_ms()
+        pgb.create_text("PRESS ANY BUTTON",20, 120,PicoGameBoy.color(0,0,0))
+        pgb.create_text("TO PLAY",50, 130, PicoGameBoy.color(0,0,0))
+        pgb.show()
+        pgb.free_mem()
+        while ticks_diff(ticks_ms(), now) < 200:
+            sleep(0.020)
+            pgb.free_mem()
+        now = ticks_ms()
 x=start_new_thread(sund, ())
 while True:
     main_game()
-    game_over()
+    pgb.fill(PicoGameBoy.color(0,0,0))
+    pgb.create_text("GAME OVER",-1, 85,PicoGameBoy.color(255,255,255))
+    pgb.text("Press A to play again.", 35, 105, PicoGameBoy.color(255,255,255))
+    pgb.text("Press home to quit.", 40, 120, PicoGameBoy.color(255,255,255))
+    pgb.create_text("Press select/start", -1, 135, PicoGameBoy.color(255,255,255))
+    pgb.create_text("to view scores.", -1, 150, PicoGameBoy.color(255,255,255))
+    pgb.create_text("Score: "+str(int(score/7)), -1, 165, PicoGameBoy.color(255,255,255))
+    pgb.show()
+    pgb.sound(110)
+    sleep(0.05)
+    pgb.sound(0)
+    sleep(0.1)
+    pgb.sound(110)
+    sleep(0.05)
+    pgb.sound(0)
+    sleep(0.1)
+    with open("highscoresflappy.sc", "r") as s:
+        scores=s.read().split("\n")
+        for r in range(len(scores)):
+            scores[r]=int(scores[r])
+    newscores=scores
+    newscores.append(int(score/10))
+    newscores.sort(reverse=True)
+    for i in range(len(newscores)): newscores[i]=str(newscores[i])
+    with open("highscoresflappy.sc", "w+") as w:
+        w.write("\n".join(newscores[:10]))
+    del newscores
+    del scores
+    while True:
+        if pgb.button_Home():
+            homebootstop=open("/noboot", "w")
+            homebootstop.close()
+            pgb.fill(PicoGameBoy.color(0,0,0))
+            pgb.show()
+            machine.reset()
+            break
+        pgb.fill(PicoGameBoy.color(0,0,0))
+        pgb.create_text("GAME OVER",-1, 85,PicoGameBoy.color(255,255,255))
+        pgb.text("Press A to play again.", 35, 105, PicoGameBoy.color(255,255,255))
+        pgb.text("Press home to quit.", 40, 120, PicoGameBoy.color(255,255,255))
+        pgb.create_text("Press select/start", -1, 135, PicoGameBoy.color(255,255,255))
+        pgb.create_text("to view scores.", -1, 150, PicoGameBoy.color(255,255,255))
+        pgb.create_text("Score: "+str(int(score/10)), -1, 165, PicoGameBoy.color(255,255,255))
+        pgb.show()
+        if pgb.button_select() or pgb.button_start():
+                x=open("highscoresflappy.sc", "r")
+                scores=x.read()
+                x.close()
+                del x
+                scores=scores.split("\n")
+                while True:
+                    if pgb.button_B():
+                        sleep(0.1)
+                        break
+                    if pgb.button_Home():
+                        homebootstop=open("/noboot", "w")
+                        homebootstop.close()
+                        pgb.fill(PicoGameBoy.color(0,0,0))
+                        pgb.show()
+                        machine.reset()
+                        break
+                    pgb.fill(PicoGameBoy.color(0,0,0))
+                    pgb.create_text("High Scores:", -1, 15, PicoGameBoy.color(255,255,255))
+                    for i in range(len(scores)):
+                        pgb.create_text("Score "+str(i+1)+": "+str(scores[i])+" Pipes", -1, 50+i*15, PicoGameBoy.color(255,255,255))
+                    pgb.create_text("Press B to exit", -1, 220, PicoGameBoy.color(255,255,255))
+                    pgb.show()
+                del scores
+        elif pgb.button_A():
+            score=0
+            break
         
