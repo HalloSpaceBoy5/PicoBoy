d6d78843a9f406b8b03a16d1c4db5aa3860df4e5
HalloSpaceBoy <111699451+HalloSpaceBoy5@users.noreply.github.com>
2023-11-19 10:49:41 -0500

    V3.1 Update

--- Data/Utils/rgb-a.py
+++ Data/Utils/rgb-a.py
@@ -8,4 +8,3 @@ png.load() # required for png.split()
 background = Image.new("RGB", png.size, (1, 1, 1))
 background.paste(png, mask=png.split()[3]) # 3 is the alpha channel
 
-background.save(name, 'PNG', quality=100)
